18fe70f570190c501c3b68f711023d85487b1678
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2023-04-30 03:35:52 +0300

    commit comm2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./Components/ChatFeed";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       projectID="086e5fdd-58f1-42d7-8a8f-498b2b6ea600"
       userName="mohammad"
       userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
---
+++ src/Components/ChatFeed.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ChatFeed = (props) => {
+  console.log(props);
+  return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
